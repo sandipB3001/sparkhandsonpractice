ffc70c1d6a0be35e408ed93668aaae069f81b27c
sandipB3001 <s.biswas3001@gmail.com>
2023-07-24 17:03:45 +0530

    commit changes

--- src/WordsCount.scala
+++ src/WordsCount.scala
@@ -1,10 +1,25 @@
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 
 object WordsCount extends App{
+  Logger.getLogger("org").setLevel(Level.ERROR)
   val sc = new SparkContext("local[*]", "wordscount")
   val textInput = sc.textFile("C:\\Users\\sandip\\Downloads\\search_data.txt")
   val words = textInput.flatMap(x => x.split(" "))
-  val wordsMap = words.map(x=> (x,1))
+  val wordsLower = words.map(x=>x.toLowerCase())
+  val wordsMap = wordsLower.map(x=> (x,1))
   val wordsCount = wordsMap.reduceByKey((x,y) => (x+y))
   wordsCount.collect.foreach(println)
+
+  //THIS WILL KEEP THE TERMINAL LIVE, SO WE WILL BE ABLE TO SEE THE DAG
+  // scala.io.StdIn.readLine()
+
+  //We can using chaining to resolve the above problem
+  /*sc.textFile("C:\\Users\\sandip\\Downloads\\search_data.txt").
+    flatMap((_.split(" "))).
+    map(_.toLowerCase()).
+    map((_,1)).
+    reduceByKey(_ + _).
+    collect.
+    foreach(println)*/
 }
