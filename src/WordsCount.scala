b8c61f64447c5f79a264804e7bc52c765495cca1
sandipB3001 <s.biswas3001@gmail.com>
2023-07-24 13:29:02 +0530

    commititng working code

--- src/WordsCount.scala
+++ src/WordsCount.scala
@@ -2,5 +2,9 @@ import org.apache.spark.SparkContext
 
 object WordsCount extends App{
   val sc = new SparkContext("local[*]", "wordscount")
-  
+  val textInput = sc.textFile("C:\\Users\\sandip\\Downloads\\search_data.txt")
+  val words = textInput.flatMap(x => x.split(" "))
+  val wordsMap = words.map(x=> (x,1))
+  val wordsCount = wordsMap.reduceByKey((x,y) => (x+y))
+  wordsCount.collect.foreach(println)
 }
