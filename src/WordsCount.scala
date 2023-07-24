22c7147235a8656cb05d183fe2482e3f158fb823
sandipB3001 <s.biswas3001@gmail.com>
2023-07-24 13:31:33 +0530

    commit

--- src/WordsCount.scala
+++ src/WordsCount.scala
@@ -6,5 +6,6 @@ object WordsCount extends App{
   val words = textInput.flatMap(x => x.split(" "))
   val wordsMap = words.map(x=> (x,1))
   val wordsCount = wordsMap.reduceByKey((x,y) => (x+y))
+  //hello
   wordsCount.collect.foreach(println)
 }
